a49cc1c4c6eed88617c5251de84bbe90ec9fe359
Alejandro Chacón <85378121+witchdrums@users.noreply.github.com>
2023-11-16 16:49:30 -0600

    Add project files.

---
+++ webapifoto/Controllers/FotoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapifoto.Models;
+
+namespace webapifoto.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FotoController : Controller
+{
+    public static IWebHostEnvironment _environment;
+
+    public FotoController(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Foto>> PostIndex([FromForm] Foto foto)
+    {
+        try
+        {
+            string webRootPath = _environment.WebRootPath;
+            string rutaArchivos = Path.Combine(webRootPath, "file");
+
+            if (foto.Archivo.Length > 0)
+            {
+                if (!Directory.Exists(rutaArchivos))
+                {
+                    Directory.CreateDirectory(rutaArchivos);
+                }
+            }
+            using (FileStream filestream = System.IO.File.Create(Path.Combine(rutaArchivos, foto.Archivo.FileName)))
+            {
+                await foto.Archivo.CopyToAsync(filestream);
+                filestream.Flush();
+            }
+            foto.Url = $"{Request.Scheme}://{Request.Host}{Request.PathBase}/file/" + foto.Archivo.FileName;
+        }
+        catch (Exception ex) 
+        {
+            return Problem(ex.Message + _environment.WebRootPath);
+        }
+        return CreatedAtAction(nameof(PostIndex), new { foto.Nombre, foto.Url });
+    }
+}
---
+++ webapifoto/Models/Foto.cs
@@ -0,0 +1,10 @@
+﻿namespace webapifoto.Models;
+
+public class Foto
+{
+    public IFormFile Archivo { get; set; }
+
+    public string Nombre { get; set; }
+
+    public string? Url { get; set; }
+}
